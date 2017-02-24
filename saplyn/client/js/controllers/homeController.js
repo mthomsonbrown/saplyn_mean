74ebc6be434829d9154c6b918fa2221cd0a8c2f7
ookamijin <mthomsonbrown@gmail.com>
2017-02-24 04:48:51 +0000

    Added a mongo connection to the server via mongoose

--- saplyn/client/js/controllers/homeController.js
+++ saplyn/client/js/controllers/homeController.js
@@ -2,6 +2,9 @@
 /*global angular*/
 
 var app = angular.module('saplyn', []);
+
 app.controller('homeController', function($scope) {
   $scope.word = "THE_BIRD";
+  
 });
+
--- saplyn/server.js
+++ saplyn/server.js
@@ -1,9 +1,11 @@
 'use-strict'
-/*global port*/
+/*global port, mongoose*/
 
 var express = require('express'),
-    app = express()
-    port = process.env.PORT; // This is specifically for Cloud9
+    app = express(),
+    port = process.env.PORT, // This is specifically for Cloud9
+    url = process.env.IP,
+    mongoose = require('mongoose');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/views/index.html');
@@ -14,5 +16,12 @@ app.use('/node', express.static(__dirname + '/node_modules'));
 
 app.listen(port, function() {
     console.log('Express is listening');
-    console.log('trying to pull angular from ' + __dirname + '/node_modules')
+});
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('We\'re connected!');
 });
